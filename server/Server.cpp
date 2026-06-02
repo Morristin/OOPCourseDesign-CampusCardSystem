345238eb950fffe15fd40f502704876755e76812
Morristin Lavender <takemehands@icloud.com>
2026-06-02 20:39:05 +0800

    refactor(server): create destructor to make sure file describer will release

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,17 +1,14 @@
 #include "Server.h"
-#include "logger/Logger.h"
+#include "../logger/Logger.h"
 
 #include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
 static constexpr auto BIND_ADDRESS_FAILED = 21;
 static constexpr auto LISTEN_FAILED = 22;
 auto logger = Logger(__FILE__);
 
-[[noreturn]] void Server::start()
+[[noreturn]] void Server::start() const
 {
-    const int server = socket(SIN_FAMILY, SOCKET_TYPE, 0);
     setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &ALLOW_PORT_REUSE, sizeof(ALLOW_PORT_REUSE));
 
     sockaddr_in server_addr { };
@@ -23,14 +20,12 @@ auto logger = Logger(__FILE__);
     if (const auto status = bind(server, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr));
         status < 0) {
         logger.critical("Can not bind address to server.");
-        close(server);
         exit(BIND_ADDRESS_FAILED);
     }
 
     // Let server start listen. Handle exceptions.
     if (const auto status = listen(server, BACKLOG); status < 0) {
         logger.critical("Can not start listen.");
-        close(server);
         exit(LISTEN_FAILED);
     }
 
--- server/Server.h
+++ server/Server.h
@@ -2,18 +2,24 @@
 #define SERVER_H
 
 #include <sys/socket.h>
+#include <unistd.h>
 
 class Server {
 private:
     static constexpr auto SIN_FAMILY = AF_INET;
     static constexpr auto SOCKET_TYPE = SOCK_STREAM;
     static constexpr auto DEFAULT_PORT = htons(8080);
-    static constexpr auto ALLOW_PORT_REUSE = 1;
 
+    static constexpr auto ALLOW_PORT_REUSE = 1;
     static constexpr auto BACKLOG = 3;
 
+    int server = socket(SIN_FAMILY, SOCKET_TYPE, 0);
+
 public:
-    [[noreturn]] static void start();
+    Server() = default;
+    ~Server() { close(server); }
+
+    [[noreturn]] void start() const;
 };
 
 #endif
