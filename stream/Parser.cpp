4481d243917067a25d6737eba601de84fb87feb3
Morristin Lavender <takemehands@icloud.com>
2026-06-03 12:27:09 +0800

    feat(parser): design a message protocal, add message parser for that

---
+++ stream/Parser.cpp
@@ -0,0 +1,22 @@
+#include "Parser.h"
+
+void Parser::store_item(const std::string& item)
+{
+    const auto colon_pos = item.find(':');
+    data[item.substr(0, colon_pos)] = item.substr(colon_pos + 1);
+}
+
+Parser::Parser(const std::string& origin_msg)
+{
+    size_t start, end = 0;
+    while ((end = origin_msg.find(',', start = (end == 0 ? 0 : end + 1))) != std::string::npos) {
+        store_item(origin_msg.substr(start, end - start));
+    }
+    store_item(origin_msg.substr(start));
+}
+
+std::string Parser::operator[](const std::string& key)
+{
+    const auto item = data.find(key);
+    return (item != data.end()) ? item->second : "";
+}
---
+++ stream/Parser.h
@@ -0,0 +1,18 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+#include <unordered_map>
+
+class Parser {
+private:
+    std::unordered_map<std::string, std::string> data;
+    void store_item(const std::string& item);
+
+public:
+    explicit Parser(const std::string& origin_msg);
+    // Operator[] won't throw any exceptions. Return blank string if key can not be found.
+    std::string operator[](const std::string& key);
+};
+
+#endif
